dea767e692170e034be065baefd08e1f5ce864f8
lucasbpro <lucasbpro@gmail.com>
2020-10-07 22:46:29 -0300

    fix: Fixed error with app.config definition

--- app.py
+++ app.py
@@ -18,8 +18,7 @@ from resources.orders import *
 app = Flask(__name__)
 
 # sets up production environment
-app.config['DEBUG'] = FALSE
-
+app.config['DEBUG'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
