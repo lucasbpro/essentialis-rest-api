ac8e38f5f32855368d55666be79da098b236569f
lucasbpro <lucasbpro@gmail.com>
2020-11-14 00:08:41 -0300

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # sets up production environment
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = 'rebequinha'
