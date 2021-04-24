e8b3dfdc6443b8bd82c25c1a6502c2c4f7e77726
lucasbpro <lucasbpro@gmail.com>
2021-04-24 20:27:22 -0300

    fix: issue with database URI due to Postgree library update

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 # sets up production environment
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db').replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'rebequinha'
 
